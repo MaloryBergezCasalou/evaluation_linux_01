62a0f1b3f45af3e3287b511a8b516319ecfc5b1c
malorybergezcasalou <malory.bergezcasalou@gmail.com>
2020-03-11 16:26:58 +0100

    version 1, 2, 4 non fini, version 3 pas reussi

--- passeplats.sh
+++ passeplats.sh
@@ -1,3 +1,23 @@
 #!/bin/bash
 
-cp -p ~/tmp/in/* /tmp/out
+touch /tmp/out/lock
+touch /tmp/out/log
+
+for * in /tmp/in/
+do
+	if [-f $/tmp/in/* ]
+	then
+		gzip $/tmp/in/*
+		mv /tmp/in/* /tmp/out/
+		echo "le(s) fichier(s) " $/tmp/out/*"bien compresse(s)" >> /tmp/out/log
+	fi
+
+	if [ -e ${LOCKFILE} ] && kill -0 cat ${LOCKFILE}; then
+    echo "Code : 22"
+    exit
+	fi
+
+done
+
+rm -rf /tmp/out/log
+rm -rf /tmp/out/lock
